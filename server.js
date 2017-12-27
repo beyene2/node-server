33460f754e8833b0e4953d1584802cbd075d8f31
Beyene Ayenew <beyenenw@gmail.com>
2017-12-27 04:12:35 +0000

    set up start server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const hbs = require("hbs");
 const fs = require("fs");
+const port = process.env.PORT || process.env.IP;
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'sbs');
